fb3a819510f61d69128d3b3c9502425bac1ca683
Tenny <joel.shu@qq.com>
2024-02-22 14:58:58 +0800

    feat: 按钮文档
    Signed-off-by: Tenny <joel.shu@qq.com>

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -48,6 +48,10 @@
         </template>
       </PcCodePreview>
     </CodeExample>
+    <h2>API</h2>
+    <PropTable :data="propData" title="Card Props"></PropTable>
+    <SlotTable :data="slotData" title="Card Slots"></SlotTable>
+    <ThemeTable :rows="varsData"></ThemeTable>
   </DocMain>
 </template>
 
@@ -57,6 +61,9 @@ import CodeExample from '../app_components/CodeExample.vue';
 import PcCodePreview from '../app_components/PcCodePreview.vue';
 import Button from '../components/Button.vue';
 import SearchIcon from '../components/SearchIcon.vue';
+import PropTable from '../app_components/PropTable.vue';
+import SlotTable from '../app_components/SlotTable.vue';
+import ThemeTable from '../app_components/ThemeTable.vue';
 
 const code1 = `
 <nt-button>Default</nt-button>
@@ -93,6 +100,134 @@ const code6 = `
 <nt-button loading>Default</nt-button>
 <nt-button type="primary" loading class="ml-10">Primary</nt-button>
 `;
+
+const propData = [
+  {
+    name: 'type',
+    description: '按钮类型, primary、text、normal',
+    default: 'normal',
+    type: 'string',
+  },
+  {
+    name: 'disabled',
+    description: '是否禁用状态',
+    default: 'false',
+    type: 'boolean',
+  },
+  {
+    name: 'htmlType',
+    description: '原生的 type 属性; button、submit、reset',
+    default: 'button',
+    type: 'string',
+  },
+  {
+    name: 'block',
+    description: '按钮是否适合其父宽度(宽度100%)',
+    default: 'false',
+    type: 'boolean',
+  },
+  {
+    name: 'circle',
+    description: '是否是圆形按钮',
+    default: 'false',
+    type: 'boolean',
+  },
+  {
+    name: 'round',
+    description: '是否是弧形按钮',
+    default: 'false',
+    type: 'boolean',
+  },
+  {
+    name: 'loading',
+    description: '是否处于加载状态',
+    default: 'false',
+    type: 'boolean',
+  },
+  {
+    name: 'loadingText',
+    description: '加载状态时显示的文字',
+    default: '加载中……',
+    type: 'string',
+  },
+];
+
+const slotData = [
+  {
+    name: 'default',
+    param: '()',
+    description: '内容区域',
+  },
+];
+
+const varsData = [
+  {
+    name: '--nt-btn-color',
+    description: '按钮的文字颜色',
+    default: 'rgba(0, 0, 0, 0.65)',
+  },
+  {
+    name: '--nt-btn-bg-color',
+    description: '按钮的背景颜色',
+    default: '#fff',
+  },
+  {
+    name: '--nt-btn-border-color',
+    description: '按钮的边框颜色',
+    default: '#d9d9d9',
+  },
+  {
+    name: '--nt-form-edit-height',
+    description: '按钮的高度',
+    default: '32px',
+  },
+  {
+    name: '--nt-btn-hover-color',
+    description: '鼠标悬浮时按钮的文字颜色',
+    default: '#79b3f7',
+  },
+  {
+    name: '--nt-btn-hover-border-color',
+    description: '鼠标悬浮时按钮的边框颜色',
+    default: 'var(--nt-btn-hover-color)',
+  },
+  {
+    name: '--nt-btn-hover-bg-color',
+    description: '鼠标悬浮时按钮的背景颜色',
+    default: 'var(--nt-btn-bg-color)',
+  },
+  {
+    name: '--nt-btn-active-color',
+    description: '点击时按钮的颜色',
+    default: '#197df1',
+  },
+  {
+    name: '--nt-btn-active-bg-color',
+    description: '点击时按钮的背景颜色',
+    default: 'var(--nt-btn-bg-color)',
+  },
+
+  {
+    name: '--nt-btn-active-border-color',
+    description: '点击时按钮的边框颜色',
+    default: 'var(--nt-btn-active-color)',
+  },
+  {
+    name: '--nt-btn-disabled-color',
+    description: '禁用时按钮的文字颜色',
+    default: '#c9c9c9',
+  },
+  {
+    name: '--nt-btn-disabled-bg-color',
+    description: '禁用时按钮的背景颜色',
+    default: '#fbfbfb',
+  },
+  {
+    name: '--nt-btn-disabled-border-color',
+    description: '禁用时按钮的边框颜色',
+    default: '#e6e6e6',
+  },
+];
 </script>
 
 <style lang="less"></style>
