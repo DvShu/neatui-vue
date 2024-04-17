a0c79a6c4c157905d22ba879d47266a9d36e3e5d
DvShu <DvShu@users.noreply.github.com>
2024-04-17 09:40:27 +0000

    deploy: ff7923e91068382017550b7e00ad2a7957c54862

---
+++ assets/components_button.md.B8kNAPXZ.lean.js
@@ -0,0 +1,25 @@
+import{x as l,N as c,c as s,f as d,w as e,a9 as r,b as t,q as o,o as b}from"./chunks/framework.frWttwNL.js";const R=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"components/button.md","filePath":"components/button.md","lastUpdated":1713345795000}'),i={name:"components/button.md"},h=r("",5),u=t("textarea",{lang:"vue-html"},`
+<nt-button>Default</nt-button>
+<nt-button type="primary" class="ml-10">Primary</nt-button>
+<nt-button type="text" class="ml-10">Text</nt-button>
+`,-1),_=t("h3",{id:"按钮禁用",tabindex:"-1"},[o("按钮禁用 "),t("a",{class:"header-anchor",href:"#按钮禁用","aria-label":'Permalink to "按钮禁用"'},"​")],-1),p=t("p",null,[o("使用 "),t("code",null,"disabled"),o(" 控制按钮禁用状态, 禁用状态下不可点击。该属性接受一个 Boolean 类型的值。")],-1),m=t("textarea",{lang:"vue-html"},`
+<nt-button disabled>Default</nt-button>
+<nt-button type="text" class="ml-10" disabled>Text</nt-button>
+`,-1),f=t("h3",{id:"按钮加载状态",tabindex:"-1"},[o("按钮加载状态 "),t("a",{class:"header-anchor",href:"#按钮加载状态","aria-label":'Permalink to "按钮加载状态"'},"​")],-1),x=t("p",null,[o("使用 "),t("code",null,"loading"),o(" 控制按钮加载状态, 加载状态下不可点击。")],-1),y=t("textarea",{lang:"vue-html"},`
+<nt-button loading>Default</nt-button>
+<nt-button type="primary" loading class="ml-10">Primary</nt-button>
+`,-1),P=t("h3",{id:"block-按钮",tabindex:"-1"},[t("code",null,"Block"),o(" 按钮 "),t("a",{class:"header-anchor",href:"#block-按钮","aria-label":'Permalink to "`Block` 按钮"'},"​")],-1),g=t("p",null,[t("code",null,"block"),o(" 属性使按钮适合其父宽度。[100%]")],-1),v=t("textarea",{lang:"vue-html"},`
+<nt-button block>Default</nt-button>
+<nt-button type="primary" block class="mt-15">Text</nt-button>
+`,-1),k=t("h3",{id:"按钮形状",tabindex:"-1"},[o("按钮形状 "),t("a",{class:"header-anchor",href:"#按钮形状","aria-label":'Permalink to "按钮形状"'},"​")],-1),T=t("p",null,"按钮可以设置为圆形或者弧形",-1),q=t("textarea",{lang:"vue-html"},`
+<nt-button circle>D</nt-button>
+<nt-button type="primary" round class="ml-10">Text</nt-button>
+`,-1),B=t("h3",{id:"图标按钮",tabindex:"-1"},[o("图标按钮 "),t("a",{class:"header-anchor",href:"#图标按钮","aria-label":'Permalink to "图标按钮"'},"​")],-1),S=t("p",null,"直接在 Button 内使用 Icon 组件。",-1),C=t("textarea",{lang:"vue-html"},`
+<nt-button circle>
+<nt-search-icon></nt-search-icon>
+</nt-button>
+<nt-button type="primary" round class="ml-10">
+<nt-search-icon></nt-search-icon>
+<span>Search</span>
+</nt-button>
+`,-1),A=r("",7);function D(N,V,I,w,$,E){const n=c("CodePreview"),a=c("ClientOnly");return b(),s("div",null,[h,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[u]),_:1})]),_:1}),_,p,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[m]),_:1})]),_:1}),f,x,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[y]),_:1})]),_:1}),P,g,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[v]),_:1})]),_:1}),k,T,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[q]),_:1})]),_:1}),B,S,d(a,null,{default:e(()=>[d(n,null,{default:e(()=>[C]),_:1})]),_:1}),A])}const J=l(i,[["render",D]]);export{R as __pageData,J as default};
---
+++ assets/components_container.md.OCmppUGI.lean.js
@@ -0,0 +1,44 @@
+import{b as i}from"./chunks/theme.BFCcLHdT.js";import{N as d,c as l,f as a,w as t,a9 as o,b as e,q as n,o as c}from"./chunks/framework.frWttwNL.js";const r=o("",9),h=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <main class="nt-main">Main</main>
+  </section>
+  `,-1),_=e("h3",{id:"左右两栏排版",tabindex:"-1"},[n("左右两栏排版 "),e("a",{class:"header-anchor",href:"#左右两栏排版","aria-label":'Permalink to "左右两栏排版"'},"​")],-1),m=e("textarea",{lang:"html"},`
+  <section class="nt-container">
+    <aside class="nt-aside">Aside</aside>
+    <main class="nt-main">Main</main>
+  </section>
+  `,-1),u=e("h3",{id:"上下两栏嵌套",tabindex:"-1"},[n("上下两栏嵌套 "),e("a",{class:"header-anchor",href:"#上下两栏嵌套","aria-label":'Permalink to "上下两栏嵌套"'},"​")],-1),p=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <section class="nt-container">
+      <aside class="nt-aside">Aside</aside>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),f=e("h3",{id:"左右两栏嵌套",tabindex:"-1"},[n("左右两栏嵌套 "),e("a",{class:"header-anchor",href:"#左右两栏嵌套","aria-label":'Permalink to "左右两栏嵌套"'},"​")],-1),b=e("textarea",{lang:"html"},`
+  <section class="nt-container">
+    <aside class="nt-aside">Aside</aside>
+    <section class="nt-container nt-layout-vertical">
+      <header class="nt-header">Header</header>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),k=e("h3",{id:"上中下三栏",tabindex:"-1"},[n("上中下三栏 "),e("a",{class:"header-anchor",href:"#上中下三栏","aria-label":'Permalink to "上中下三栏"'},"​")],-1),x=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <main class="nt-main">Main</main>
+    <footer class="nt-footer">Footer</footer>
+  </section>
+  `,-1),g=e("h3",{id:"经典后台系统",tabindex:"-1"},[n("经典后台系统 "),e("a",{class:"header-anchor",href:"#经典后台系统","aria-label":'Permalink to "经典后台系统"'},"​")],-1),v=e("p",null,[n("在 "),e("code",null,"header"),n(" 里面放点东西就成了，就成了经典的后台管理系统的模板, 左边是图标，右边是登录用户；"),e("code",null,"Aside"),n(" 里面放菜单("),e("code",null,"Menu"),n(")，"),e("code",null,"Main"),n(" 里面放内容就是经典的后台管理系统模板")],-1),P=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">
+      <a href="#">NEATUI</a>
+      <div>登录用户</div>
+    </header>
+    <section class="nt-container">
+      <aside class="nt-aside">Aside</aside>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),y=o("",3),M=JSON.parse('{"title":"Container 容器布局","description":"","frontmatter":{},"headers":[],"relativePath":"components/container.md","filePath":"components/container.md","lastUpdated":1713345795000}'),C={name:"components/container.md"},S=Object.assign(C,{setup(T){return(q,A)=>{const s=d("ClientOnly");return c(),l("div",null,[r,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[h]),_:1})]),_:1}),_,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[m]),_:1})]),_:1}),u,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[p]),_:1})]),_:1}),f,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[b]),_:1})]),_:1}),k,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[x]),_:1})]),_:1}),g,v,a(s,null,{default:t(()=>[a(i,null,{default:t(()=>[P]),_:1})]),_:1}),y])}}});export{M as __pageData,S as default};
