c466895a1d42c8bb32883f10f19f45c6c700b66d
Tenny <tenny.shu@foxmail.com>
2024-04-18 16:45:19 +0800

    chore: 构建工具更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
+import { readdirSync } from 'node:fs';
+import path from 'path';
+
+const entries = {};
+
+const entryFiles = readdirSync('./src', { recursive: true, encoding: 'utf-8' });
+for (const filename of entryFiles) {
+  if (filename.startsWith('components') || filename.startsWith('directives')) {
+    const filepath = path.parse(filename);
+    if (filepath.ext === '') continue;
+    const fileAbsName = path.join('src', filename);
+    if (filepath.dir === `components${path.sep}Message`) {
+      if (filepath.ext === '.ts') {
+        entries[filepath.dir] = fileAbsName;
+      }
+    } else {
+      entries[path.join(filepath.dir, filepath.name)] = fileAbsName;
+    }
+  }
+}
+entries['index'] = 'src/index.ts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,15 +34,20 @@ export default defineConfig({
   build: {
     outDir: 'lib',
     lib: {
-      entry: 'src/index.ts',
+      entry: entries,
       formats: ['es'],
-      fileName: 'index',
+      fileName: (format, entryName) => {
+        return `${entryName}.js`;
+      },
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue', 'ph-utils'],
+      output: {
+        chunkFileNames: '[name]-[hash].js',
+      },
     },
-    emptyOutDir: false,
+    emptyOutDir: true,
     copyPublicDir: false,
   },
 });
