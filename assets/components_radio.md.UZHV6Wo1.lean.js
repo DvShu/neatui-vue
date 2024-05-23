35c128284ceb8159a049b933f28c7ec713b48fda
DvShu <DvShu@users.noreply.github.com>
2024-05-23 02:07:21 +0000

    deploy: 3e557a16afd208d4f47aa39c1981543daede8786

---
+++ assets/components_radio.md.UZHV6Wo1.lean.js
@@ -0,0 +1,36 @@
+import{h as n,i as u}from"./chunks/theme.D7s38EFP.js";import{e as p,c as m,g as e,w as a,ab as b,b as t,x as d,R as _,o as v,y as o}from"./chunks/framework.kESMpjgr.js";const f=b("",5),x=t("textarea",{lang:"vue"},`
+  <script setup>
+    import { ref } from 'vue';
+    const allowService = ref(false);
+  <\/script>
+  <template>
+    <nt-radio v-model="allowService">
+      <template #label>
+        <span>
+          <span>同意</span>
+          <a href='#'>隐私协议</a>
+        </span>
+      </template>
+    </nt-radio>
+  </template>
+  `,-1),P=t("span",null,[t("span",null,"同意"),t("a",{href:"#"},"隐私协议")],-1),g=t("h3",{id:"禁用状态",tabindex:"-1"},[d("禁用状态 "),t("a",{class:"header-anchor",href:"#禁用状态","aria-label":'Permalink to "禁用状态"'},"​")],-1),y=t("p",null,[d("设置 "),t("code",null,"disabled"),d(" 属性即可。")],-1),q=t("textarea",{lang:"vue-html"},`
+  <nt-radio label="禁用" disabled>
+  `,-1),V=t("h3",{id:"单选框组",tabindex:"-1"},[d("单选框组 "),t("a",{class:"header-anchor",href:"#单选框组","aria-label":'Permalink to "单选框组"'},"​")],-1),k=t("p",null,[d("结合 "),t("code",null,"nt-radio-group"),d(" 组件实现单选框组")],-1),R=t("textarea",{lang:"vue-html"},`
+  <script setup>
+    import { ref } from 'vue';
+    const sex = ref('0');
+  <\/script>
+  <template>
+    <nt-radio-group v-model="sex">
+      <nt-radio label="男" value="0" />
+      <nt-radio label="女" value="1" />
+    </nt-radio-group>
+  </template>
+  `,-1),S=t("h3",{id:"按钮样式",tabindex:"-1"},[d("按钮样式 "),t("a",{class:"header-anchor",href:"#按钮样式","aria-label":'Permalink to "按钮样式"'},"​")],-1),T=t("p",null,[d("在单选框组基础上，设置 "),t("code",null,"nt-radio"),d(" 的 "),t("code",null,"type"),d(" 属性为 "),t("code",null,"button"),d(" 即可。")],-1),w=t("textarea",{lang:"vue-html"},`
+  <template>
+    <nt-radio-group v-model="sex">
+      <nt-radio label="男" value="0" type="button" />
+      <nt-radio label="女" value="1" type="button" />
+    </nt-radio-group>
+  </template>
+  `,-1),C=b("",11),O=JSON.parse('{"title":"Radio 单选框","description":"","frontmatter":{},"headers":[],"relativePath":"components/radio.md","filePath":"components/radio.md","lastUpdated":1716362007000}'),A={name:"components/radio.md"},U=Object.assign(A,{setup(E){const i=p("0"),h=p(!1);return(N,l)=>{const s=_("CodePreview"),c=_("ClientOnly");return v(),m("div",null,[f,e(c,null,{default:a(()=>[e(s,null,{preview:a(()=>[e(o(n),{modelValue:h.value,"onUpdate:modelValue":l[0]||(l[0]=r=>h.value=r)},{label:a(()=>[P]),_:1},8,["modelValue"])]),default:a(()=>[x]),_:1})]),_:1}),g,y,e(c,null,{default:a(()=>[e(s,null,{default:a(()=>[q]),_:1})]),_:1}),V,k,e(c,null,{default:a(()=>[e(s,null,{preview:a(()=>[e(o(u),{modelValue:i.value,"onUpdate:modelValue":l[1]||(l[1]=r=>i.value=r)},{default:a(()=>[e(o(n),{label:"男",value:"0"}),e(o(n),{label:"女",value:"1"})]),_:1},8,["modelValue"])]),default:a(()=>[R]),_:1})]),_:1}),S,T,e(c,null,{default:a(()=>[e(s,null,{preview:a(()=>[e(o(u),{modelValue:i.value,"onUpdate:modelValue":l[2]||(l[2]=r=>i.value=r)},{default:a(()=>[e(o(n),{label:"男",value:"0",type:"button"}),e(o(n),{label:"女",value:"1",type:"button"})]),_:1},8,["modelValue"])]),default:a(()=>[w]),_:1})]),_:1}),C])}}});export{O as __pageData,U as default};
---
+++ assets/components_switch.md.D0fYYD2c.lean.js
@@ -0,0 +1,13 @@
+import{j as r}from"./chunks/theme.D7s38EFP.js";import{e as _,c as p,g as e,w as a,ab as u,b as t,x as c,R as s,o as m,y as i}from"./chunks/framework.kESMpjgr.js";const b=u("",5),w=t("textarea",{lang:"vue"},`
+  <script setup>
+    import { ref } from 'vue';
+    const open = ref(true);
+  <\/script>
+  <template>
+    <nt-switch v-model="open"></nt-switch>
+  </template>
+  `,-1),f=t("h3",{id:"文字描述",tabindex:"-1"},[c("文字描述 "),t("a",{class:"header-anchor",href:"#文字描述","aria-label":'Permalink to "文字描述"'},"​")],-1),v=t("p",null,[c("使用 "),t("code",null,"checked-text"),c(" 和 "),t("code",null,"unchecked-text"),c(" 属性来设置开关的文字描述。")],-1),x=t("textarea",{lang:"vue"},`
+  <template>
+    <nt-switch v-model="open" checked-text="Y" unchecked-text="N"></nt-switch>
+  </template>
+  `,-1),S=u("",7),C=JSON.parse('{"title":"Switch 开关","description":"","frontmatter":{},"headers":[],"relativePath":"components/switch.md","filePath":"components/switch.md","lastUpdated":1716430006000}'),P={name:"components/switch.md"},g=Object.assign(P,{setup(k){const d=_(!0);return(V,o)=>{const h=s("CodePreview"),l=s("ClientOnly");return m(),p("div",null,[b,e(l,null,{default:a(()=>[e(h,null,{preview:a(()=>[e(i(r),{modelValue:d.value,"onUpdate:modelValue":o[0]||(o[0]=n=>d.value=n)},null,8,["modelValue"])]),default:a(()=>[w]),_:1})]),_:1}),f,v,e(l,null,{default:a(()=>[e(h,null,{preview:a(()=>[e(i(r),{modelValue:d.value,"onUpdate:modelValue":o[1]||(o[1]=n=>d.value=n),"checked-text":"Y","unchecked-text":"N"},null,8,["modelValue"])]),default:a(()=>[x]),_:1})]),_:1}),S])}}});export{C as __pageData,g as default};
