f18a31f487d7c16906290f5e561bd3aa187ba602
Tenny <tenny.shu@foxmail.com>
2024-06-07 09:53:18 +0800

    fix(Table): 优化表格
    fix(Icon): 增加 ArrowUp、ArrowDown 组件

---
+++ src/components/icon/ArrowDown.vue
@@ -0,0 +1,12 @@
+<template>
+  <BaseIcon>
+    <path
+      fill="currentColor"
+      d="M831.872 340.864 512 652.672 192.128 340.864a30.592 30.592 0 0 0-42.752 0 29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728 30.592 30.592 0 0 0-42.752 0z"
+    ></path>
+  </BaseIcon>
+</template>
+
+<script setup lang="ts">
+import BaseIcon from './Base.vue';
+</script>
