56c5fcfc49f0a4c6047e3545a7cfc6cd790525d6
DvShu <DvShu@users.noreply.github.com>
2024-06-14 16:55:09 +0000

    deploy: 6aef628e8f8ccda2dcd1e1efb401413349ff0425

---
+++ assets/components_container.md.CThXx1OG.lean.js
@@ -0,0 +1,44 @@
+import{e as i}from"./chunks/theme.699TBE5S.js";import{c as d,k as a,w as t,aa as o,b as e,y as n,R as l,o as c}from"./chunks/framework.Yy9K0PbY.js";const r=o("",9),h=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <main class="nt-main">Main</main>
+  </section>
+  `,-1),_=e("h3",{id:"左右两栏排版",tabindex:"-1"},[n("左右两栏排版 "),e("a",{class:"header-anchor",href:"#左右两栏排版","aria-label":'Permalink to "左右两栏排版"'},"​")],-1),m=e("textarea",{lang:"html"},`
+  <section class="nt-container">
+    <aside class="nt-aside">Aside</aside>
+    <main class="nt-main">Main</main>
+  </section>
+  `,-1),u=e("h3",{id:"上下两栏嵌套",tabindex:"-1"},[n("上下两栏嵌套 "),e("a",{class:"header-anchor",href:"#上下两栏嵌套","aria-label":'Permalink to "上下两栏嵌套"'},"​")],-1),p=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <section class="nt-container">
+      <aside class="nt-aside">Aside</aside>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),f=e("h3",{id:"左右两栏嵌套",tabindex:"-1"},[n("左右两栏嵌套 "),e("a",{class:"header-anchor",href:"#左右两栏嵌套","aria-label":'Permalink to "左右两栏嵌套"'},"​")],-1),b=e("textarea",{lang:"html"},`
+  <section class="nt-container">
+    <aside class="nt-aside">Aside</aside>
+    <section class="nt-container nt-layout-vertical">
+      <header class="nt-header">Header</header>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),x=e("h3",{id:"上中下三栏",tabindex:"-1"},[n("上中下三栏 "),e("a",{class:"header-anchor",href:"#上中下三栏","aria-label":'Permalink to "上中下三栏"'},"​")],-1),k=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">Header</header>
+    <main class="nt-main">Main</main>
+    <footer class="nt-footer">Footer</footer>
+  </section>
+  `,-1),g=e("h3",{id:"经典后台系统",tabindex:"-1"},[n("经典后台系统 "),e("a",{class:"header-anchor",href:"#经典后台系统","aria-label":'Permalink to "经典后台系统"'},"​")],-1),v=e("p",null,[n("在 "),e("code",null,"header"),n(" 里面放点东西就成了，就成了经典的后台管理系统的模板, 左边是图标，右边是登录用户；"),e("code",null,"Aside"),n(" 里面放菜单("),e("code",null,"Menu"),n(")，"),e("code",null,"Main"),n(" 里面放内容就是经典的后台管理系统模板")],-1),y=e("textarea",{lang:"html"},`
+  <section class="nt-container nt-layout-vertical">
+    <header class="nt-header">
+      <a href="#">NEATUI</a>
+      <div>登录用户</div>
+    </header>
+    <section class="nt-container">
+      <aside class="nt-aside">Aside</aside>
+      <main class="nt-main">Main</main>
+    </section>
+  </section>
+  `,-1),P=o("",3),N=JSON.parse('{"title":"Container 容器布局","description":"","frontmatter":{},"headers":[],"relativePath":"components/container.md","filePath":"components/container.md","lastUpdated":1714270621000}'),C={name:"components/container.md"},S=Object.assign(C,{setup(T){return(A,q)=>{const s=l("ClientOnly");return c(),d("div",null,[r,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[h]),_:1})]),_:1}),_,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[m]),_:1})]),_:1}),u,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[p]),_:1})]),_:1}),f,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[b]),_:1})]),_:1}),x,a(s,null,{default:t(()=>[a(i,{class:"mt-15"},{default:t(()=>[k]),_:1})]),_:1}),g,v,a(s,null,{default:t(()=>[a(i,null,{default:t(()=>[y]),_:1})]),_:1}),P])}}});export{N as __pageData,S as default};
