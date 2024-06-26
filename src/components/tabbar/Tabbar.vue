bc7ed1fb05a2aeca1548395fe2d239d5e5ff639a
Tenny <joel.shu@qq.com>
2024-06-26 23:35:39 +0800

    创建Tabbar组件

---
+++ src/components/tabbar/Tabbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nt-tabbar" :class="['nt-tabbar--' + type]"><slot></slot></div>
+</template>
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    /**
+     * 标签类型:
+     *  1. bar - 每一项等分排列, 类似于移动端底部的标签栏
+     */
+    type?: 'bar';
+  }>(),
+  {
+    type: 'bar',
+  },
+);
+</script>
---
+++ src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="nt-tabbar-item active"><slot></slot></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
