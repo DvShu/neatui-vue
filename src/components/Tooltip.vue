2987cfba8ad13a574f22fa2f4a7fdedafd6ecb28
Tenny <joel.shu@qq.com>
2024-07-04 23:36:01 +0800

    fix(Tooltip): 提示增加点击弹出

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,62 +1,107 @@
-<template>
-  <div
-    :class="{
-      'nt-tooltip': true,
-      'nt-tooltip-hover': trigger === 'hover',
-      'nt-tooltip-visible': visible || open,
-    }"
-    @click="handleClick"
-  >
-    <slot></slot>
-    <div
-      :class="[
-        'nt-tooltip-content',
-        `nt-tooltip--${styleName}`,
-        placement.startsWith('top') ? 'nt-tooltip-top' : '',
-        placement.startsWith('bottom') ? 'nt-tooltip-bottom' : '',
-        placement.endsWith('Start') ? 'nt-tooltip-start' : '',
-        placement.endsWith('End') ? 'nt-tooltip-end' : '',
-        contentClass,
-      ]"
-    >
-      <slot name="title">{{ title }}</slot>
-    </div>
-  </div>
-</template>
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
+import { defineComponent, h, ref, withDirectives } from 'vue';
+import type { PropType } from 'vue';
+import Clickoutsize from '../directives/clickoutside';
 
-const props = withDefaults(
-  defineProps<{
-    title?: string;
+export default defineComponent({
+  props: {
+    /** 提示文本 */
+    title: String,
     /** 提示框的边框位置, 默认为 'top' */
-    placement?:
-      | 'topStart'
-      | 'top'
-      | 'topEnd'
-      | 'bottomStart'
-      | 'bottom'
-      | 'bottomEnd';
+    placement: {
+      type: String as PropType<
+        'topStart' | 'top' | 'topEnd' | 'bottomStart' | 'bottom' | 'bottomEnd'
+      >,
+      required: false,
+      default: 'top',
+    },
     /** 组件可见性, 受控模式 */
-    visible?: boolean;
+    visible: {
+      type: Boolean,
+      default: false,
+    },
     /** Tooltip Content 样式 */
-    contentClass?: string;
-    styleName?: string;
-    trigger?: 'hover' | 'click';
-  }>(),
-  {
-    placement: 'top',
-    visible: false,
-    styleName: 'default',
-    trigger: 'hover',
+    contentClass: String,
+    styleName: {
+      type: String,
+      default: 'default',
+    },
+    /** 触发方式 */
+    trigger: {
+      type: String as PropType<'hover' | 'click'>,
+      default: 'hover',
+    },
   },
-);
+  setup(props, { slots }) {
+    const open = ref(false);
 
-const open = ref(false);
+    function handleClick(e: Event) {
+      if (props.trigger === 'click') {
+        if (!open.value) {
+          open.value = true;
+        } else {
+          let $target = e.target as HTMLElement;
+          let isContent = false;
+          while (true) {
+            const classList = $target.classList;
+            if (classList.contains('nt-tooltip-content')) {
+              isContent = true;
+              break;
+            } else if (classList.contains('nt-tooltip')) {
+              break;
+            }
+            $target = $target.parentElement as HTMLElement;
+          }
+          // 点击的不是提示内容, 则隐藏提示
+          if (!isContent) {
+            open.value = false;
+          }
+        }
+      }
+    }
 
-function handleClick() {
-  if (props.trigger === 'click') {
-    open.value = !open.value;
-  }
-}
+    function handleOutsideClick() {
+      if (props.trigger === 'click') {
+        open.value = false;
+      }
+    }
+    return () => {
+      const prop: any = {
+        class: {
+          'nt-tooltip': true,
+          'nt-tooltip-hover': props.trigger === 'hover',
+          'nt-tooltip-visible': props.visible || open.value,
+        },
+      };
+      if (props.trigger === 'click') {
+        prop.onClick = handleClick;
+      }
+      const children = [
+        slots.default != null ? slots.default() : null,
+        h(
+          'div',
+          {
+            class: [
+              'nt-tooltip-content',
+              `nt-tooltip--${props.styleName}`,
+              props.placement.startsWith('top') ? 'nt-tooltip-top' : '',
+              props.placement.startsWith('bottom') ? 'nt-tooltip-bottom' : '',
+              props.placement.endsWith('Start') ? 'nt-tooltip-start' : '',
+              props.placement.endsWith('End') ? 'nt-tooltip-end' : '',
+              props.contentClass,
+            ],
+          },
+          slots.title != null ? slots.title() : props.title,
+        ),
+      ];
+      if (props.trigger === 'hover') {
+        return h('div', prop, children);
+      } else {
+        return withDirectives(h('div', prop, children), [
+          [Clickoutsize, handleOutsideClick],
+        ]);
+      }
+    };
+  },
+});
 </script>
