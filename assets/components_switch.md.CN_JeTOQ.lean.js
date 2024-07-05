33220148a30e714b6617df182cc31562abce9052
DvShu <DvShu@users.noreply.github.com>
2024-07-05 07:21:14 +0000

    deploy: 31052baebc9fe731481cd5b94afe591734721d30

---
+++ assets/components_switch.md.CN_JeTOQ.lean.js
@@ -0,0 +1,13 @@
+import{q as r}from"./chunks/theme.BseZDwuB.js";import{e as _,c as p,l as e,w as a,ab as u,b as t,G as c,U as s,o as m,z as i}from"./chunks/framework.MCgil6G4.js";const b=u("",5),w=t("textarea",{lang:"vue"},`
+  <script setup>
+    import { ref } from 'vue';
+    const open = ref(true);
+  <\/script>
+  <template>
+    <nt-switch v-model="open"></nt-switch>
+  </template>
+  `,-1),f=t("h3",{id:"文字描述",tabindex:"-1"},[c("文字描述 "),t("a",{class:"header-anchor",href:"#文字描述","aria-label":'Permalink to "文字描述"'},"​")],-1),x=t("p",null,[c("使用 "),t("code",null,"checked-text"),c(" 和 "),t("code",null,"unchecked-text"),c(" 属性来设置开关的文字描述。")],-1),v=t("textarea",{lang:"vue"},`
+  <template>
+    <nt-switch v-model="open" checked-text="Y" unchecked-text="N"></nt-switch>
+  </template>
+  `,-1),S=u("",7),C=JSON.parse('{"title":"Switch 开关","description":"","frontmatter":{},"headers":[],"relativePath":"components/switch.md","filePath":"components/switch.md","lastUpdated":1716430006000}'),P={name:"components/switch.md"},g=Object.assign(P,{setup(k){const d=_(!0);return(V,o)=>{const h=s("CodePreview"),l=s("ClientOnly");return m(),p("div",null,[b,e(l,null,{default:a(()=>[e(h,null,{preview:a(()=>[e(i(r),{modelValue:d.value,"onUpdate:modelValue":o[0]||(o[0]=n=>d.value=n)},null,8,["modelValue"])]),default:a(()=>[w]),_:1})]),_:1}),f,x,e(l,null,{default:a(()=>[e(h,null,{preview:a(()=>[e(i(r),{modelValue:d.value,"onUpdate:modelValue":o[1]||(o[1]=n=>d.value=n),"checked-text":"Y","unchecked-text":"N"},null,8,["modelValue"])]),default:a(()=>[v]),_:1})]),_:1}),S])}}});export{C as __pageData,g as default};
