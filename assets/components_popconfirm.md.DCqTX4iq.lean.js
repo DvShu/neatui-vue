f2c1423ffb3661219a0239576a1610f5696b72fe
DvShu <DvShu@users.noreply.github.com>
2024-08-08 09:25:11 +0000

    deploy: 1df228a033d677c703aa806393f1735c68352405

--- assets/components_popconfirm.md.DCqTX4iq.lean.js
+++
@@ -1,7 +0,0 @@
-import{x as c,_ as i}from"./chunks/theme.CjnrSd6H.js";import{o as l,c as s,g as e,w as t,ae as d,Y as o,l as a,K as h,b as p}from"./chunks/framework.zsgsDjJh.js";const _=d("",5),m=p("textarea",{lang:"vue-html"},`
-  <nt-popconfirm title="确认要删除吗？">
-    <template #trigger>
-      <nt-button>删除</nt-button>
-    </template>
-  </nt-popconfirm>
-  `,-1),f=d("",7),v=JSON.parse('{"title":"Popconfirm","description":"","frontmatter":{},"headers":[],"relativePath":"components/popconfirm.md","filePath":"components/popconfirm.md","lastUpdated":1721968438000}'),b={name:"components/popconfirm.md"},T=Object.assign(b,{setup(u){return(P,x)=>{const r=o("CodePreview"),n=o("ClientOnly");return l(),s("div",null,[_,e(n,null,{default:t(()=>[e(r,null,{preview:t(()=>[e(a(c),{title:"确认要删除吗？"},{trigger:t(()=>[e(a(i),null,{default:t(()=>[h("删除")]),_:1})]),_:1})]),default:t(()=>[m]),_:1})]),_:1}),f])}}});export{v as __pageData,T as default};
--- assets/components_tag.md.DuFpMNbL.lean.js
+++
@@ -1,4 +0,0 @@
-import"./chunks/theme.CjnrSd6H.js";import{o as c,c as n,g as t,w as a,ae as o,Y as e,b as l}from"./chunks/framework.zsgsDjJh.js";const s=o("",5),h=l("textarea",{lang:"vue-html"},`
-  <nt-tag>标签</nt-tag>
-  <nt-tag class="ml-10" color="#409eff">标签</nt-tag>
-  `,-1),i=o("",5),f=JSON.parse('{"title":"Tag 标签","description":"","frontmatter":{},"headers":[],"relativePath":"components/tag.md","filePath":"components/tag.md","lastUpdated":1722397676000}'),_={name:"components/tag.md"},P=Object.assign(_,{setup(p){return(b,m)=>{const d=e("CodePreview"),r=e("ClientOnly");return c(),n("div",null,[s,t(r,null,{default:a(()=>[t(d,null,{default:a(()=>[h]),_:1})]),_:1}),i])}}});export{f as __pageData,P as default};
