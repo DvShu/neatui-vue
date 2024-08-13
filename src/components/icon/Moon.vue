8a3a17926b4e6b845ddcaa4d49c6e9163059ecc1
Tenny <tenny.shu@foxmail.com>
2024-08-13 17:44:53 +0800

    xx

---
+++ src/components/icon/Moon.vue
@@ -0,0 +1,17 @@
+<template>
+  <Base
+    view-box="0 0 24 24"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+  >
+    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+  </Base>
+</template>
+
+<script setup lang="ts">
+import Base from './Base.vue';
+</script>
+
+<style lang="less"></style>
