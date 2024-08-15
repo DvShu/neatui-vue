c8f1667c4d8f637042823ae682e05f59512207fd
Tenny <tenny.shu@foxmail.com>
2024-08-15 18:07:25 +0800

    ci: 优化脚本
    Signed-off-by: Tenny <tenny.shu@foxmail.com>

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,1 +1,13 @@
-Write-Host '您好'
+param ($Cmd = $(throw "Command parameter is required."))
+
+if ($Cmd -eq "push") {
+  Write-Host "push..."
+} elseif (($Cmd -eq "pull") -or ($Cmd -eq "publish")) {
+  git pull origin main
+  pnpm lib:build
+  npm publish --registry https://registry.npmjs.org/
+  git checkout dev
+  git merge main
+
+  Write-Host " "
+}
