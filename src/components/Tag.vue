412f5815bed69fbf49e0c9327a4ca1bfffe8aeb7
Tenny <tenny.shu@foxmail.com>
2024-11-08 11:14:02 +0800

    fix(Tag): 标签可移除

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -2,6 +2,9 @@
   <Transition appear name="nt-scale">
     <div :class="['nt-tag', `nt-tag--${type}`]" :style="styleObj">
       <div class="nt-tag--content"><slot></slot></div>
+      <i v-if="closable" class="nt-tag--close" @click="handleClose">
+        <Close></Close>
+      </i>
     </div>
   </Transition>
 </template>
@@ -9,15 +12,26 @@
 import { computed } from 'vue';
 import { adjust } from 'ph-utils/color';
 import { isBlank } from 'ph-utils';
+import Close from './icon/Close.vue';
+
 const props = withDefaults(
   defineProps<{
     type?: 'primary' | 'error' | 'success';
     color?: string;
+    /** 是否可关闭 */
+    closable?: boolean;
   }>(),
   {
     type: 'primary',
+    closable: false,
   },
 );
+
+const emits = defineEmits(['close']);
+
+function handleClose() {
+  emits('close');
+}
 
 const styleObj = computed(() => {
   let obj: any = {};
